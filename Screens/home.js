9d6755f31b12fc01c197a847d55c6c1749eb320a
Reekoru <juanenriquejugo@gmail.com>
2021-05-14 09:20:31 +1200

    .

--- Screens/home.js
+++ Screens/home.js
@@ -11,42 +11,80 @@ import {
   TextInput,
   Image,
   Header,
+  Dimensions,
+  Platform,
 } from "react-native";
 
 export default function Home() {
-  console.log("App Executed");
+  /**
+   * Returns true if the screen is in portrait mode
+   */
+  const isPortrait = () => {
+    const dim = Dimensions.get("screen");
+    return dim.height >= dim.width;
+  };
 
+  /**
+   * Returns true of the screen is in landscape mode
+   */
+  const isLandscape = () => {
+    const dim = Dimensions.get("screen");
+    return dim.width >= dim.height;
+  };
+
+  this.state = {
+    orientation: isPortrait() ? "portrait" : "landscape",
+  };
+
+  // Event Listener for orientation changes
+  Dimensions.addEventListener("change", () => {
+    this.setState({
+      orientation: Platform.isPortrait() ? "portrait" : "landscape",
+    });
+  });
+
+  if (this.state.orientation === "portrait") {
+    console.log(1);
+  } else if (this.state.orientation === "landscape") {
+    console.log(0);
+  }
   return (
-    <ScrollView style={styles.container} contentContainerStyle={{alignItems: "center"}}>
-      <SafeAreaView style={styles.header}>
-        <Text style={styles.headerText}>Guitarista</Text>
-      </SafeAreaView>
+    <View style={styles.container}>
+      <ScrollView
+        style={styles.content}
+        contentContainerStyle={{ alignItems: "center" }}
+      >
+        <Text style={styles.headerText}>WELCOME</Text>
+        <Image
+          style={styles.tabs}
+          source={{
+            width: 300,
+            height: 200,
+            uri: "https://picsum.photos/300/200",
+          }}
+        />
 
-      <Image style={styles.tabs}
-      source={{
-        width: 300,
-        height: 200,
-        uri: "https://picsum.photos/300/200"
-      }}
-      />
+        <Image
+          style={styles.tabs}
+          source={{
+            width: 300,
+            height: 200,
+            uri: "https://picsum.photos/300/200",
+          }}
+        />
 
-    <Image style={styles.tabs}
-      source={{
-        width: 300,
-        height: 200,
-        uri: "https://picsum.photos/300/200"
-      }}
-      />
+        <Image
+          style={styles.tabs}
+          source={{
+            width: 300,
+            height: 200,
+            uri: "https://picsum.photos/300/200",
+          }}
+        />
+      </ScrollView>
 
-    <Image style={styles.tabs}
-      source={{
-        width: 300,
-        height: 200,
-        uri: "https://picsum.photos/300/200"
-      }}
-      />
-
-    </ScrollView>
+      <View style={styles.footer}></View>
+    </View>
   );
 }
 
@@ -54,10 +92,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "rgb(50, 50, 50)",
-
+    paddingTop: 50,
   },
 
-  header: {
+  footer: {
     backgroundColor: "rgb(40, 40, 40)",
     height: "10%",
     width: "100%",
@@ -69,7 +107,6 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     fontSize: 20,
     color: "white",
-    margin: 20,
     fontFamily: "Copperplate",
   },
 
